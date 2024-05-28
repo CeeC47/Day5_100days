db393d1f3e1b89b055638647e0209ae7a1da5755
CeeC47 <147382922+CeeC47@users.noreply.github.com>
2024-05-28 16:28:48 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Marvel Movie Character Creator")
+print("--")
+ans1 = input("Do you like 'hanging around'?")
+if ans1 == "No":
+  print("Then you're not Spider-man")
+else:
+  print("You are Spider-man")
+ans2 = input("Do you have a 'gravelly' voice?")
+if ans2 == "No":
+  print("Aww, then you're not Korg")
+else:
+  print("You are Korg")
+ans3 = input("Do you often feel 'Marvelous'?")
+if ans3 == "Yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("You are not Captain Marvel")
